e199305e6e0ecbf8b9aa684346775d8e46c63142
Melanie Schicker <melanie.schicker@gmail.com>
2019-06-11 22:15:58 +0000

    Automatically backed up by Learn

---
+++ conditionals.rb
@@ -0,0 +1,13 @@
+dog = "cuddly"
+ 
+if dog == "hungry"
+  puts "Refilling food bowl."
+elsif dog == "thirsty"
+  puts "Refilling water bowl."
+elsif dog == "playful"
+  puts "Playing tug-of-war."
+elsif dog == "cuddly"
+  puts "Snuggling."
+else
+  puts "Reading newspaper."
+end
---
+++ while_until_loops.rb
@@ -0,0 +1,11 @@
+counter = 0 
+while counter < 20
+  puts "The current number is less than 20."
+  counter += 1 
+end
+
+counter = 0
+until counter == 20
+  puts "The current number is less than 20."
+  counter += 1
+end
